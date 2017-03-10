12ec62448362d62be93f269a1372fb27d628445e
Muigai <muigai@Muigais-MacBook-Air.local>
2017-03-10 12:24:59 -0500

    updated expenses and transactions layout

---
+++ public/views/login/LoginController.js
@@ -0,0 +1,51 @@
+/**
+ * Created by muigai on 2/23/17.
+ */
+(function() {
+    angular
+        .module("BeautyBooks")
+        .controller("LoginController", LoginController);
+
+    // arguments will include a Login Service, and $location
+    function LoginController($location) {
+        let vm = this;
+        let open;
+        vm.login = login;
+        vm.learnMore = learnMore;
+
+
+        function init() {
+            console.log("loaded");
+            open = false;
+        }
+        init();
+
+        function login(e) {
+
+            let loginType = e.currentTarget.id;
+            if (loginType == "login-facebook") {
+                // Login code for facebook then route to this location on success
+                console.log("Login with Facebook");
+                $location.url("/home/");
+            } else if (loginType == "login-google") {
+                // Login code for Google then route to this location on success
+                console.log("Login with Google");
+                $location.url("/home/");
+            }
+        }
+
+        function learnMore() {
+            var drawer = document.getElementsByClassName("drawer");
+
+            open = !open;
+            if (open) {
+                Velocity(drawer, {translateY: '50%'}, {duration: 500, easing: "easeInOutQuart"});
+            } else {
+                Velocity(drawer, {translateY: '150%'}, {duration: 500, easing: "easeInOutQuart"});
+            }
+        }
+
+    }
+
+    
+})();
---
+++ public/views/transactions/TransactionsController.js
@@ -0,0 +1,18 @@
+/**
+ * Created by muigai on 2/23/17.
+ */
+(function() {
+    angular
+        .module("BeautyBooks")
+        .controller("TransactionsAddedController", TransactionsAddedController)
+        .controller("TransactionsReviewedController", TransactionsReviewedController);
+
+    function TransactionsAddedController() {
+
+    }
+
+    function TransactionsReviewedController() {
+
+    }
+
+})();
