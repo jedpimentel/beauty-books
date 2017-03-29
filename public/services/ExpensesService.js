edb9dbf0bdc540bfef5d47ff74bc2cba984c68df
Muigai <muigai@Muigais-MacBook-Air.local>
2017-03-29 10:29:45 -0400

    added expenses service

---
+++ public/services/ExpensesService.js
@@ -0,0 +1,35 @@
+(function(){
+    angular
+        .module("BeautyBooks")
+        .service("ExpenseService", ExpenseServices);
+
+    function ExpenseServices($http){
+        var api = {
+            "createexpense": createExpense,
+            "getExpenses": getExpenses,
+            "updateExpense": updateExpense,
+            "deleteExpense": deleteExpense
+        };
+        return api;
+
+        function createExpense(expense){
+            return $http.post('/api/expense', expense);
+        }
+
+        function getExpenses(expense) {
+            return $http.get('/api/expense' + expenseId);
+        }
+        
+        function getExpenseById() {
+            
+        }
+
+        function updateExpense(expenseId){
+            return $http.put('/api/expense/'+expenseId, expense);
+        }
+
+        function deleteExpense(expenseId){
+            return $http.delete('/api/expense', expenseId);
+        }
+    }
+})();
