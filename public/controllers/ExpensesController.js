69602fb4e97a3c823c56cebaf9cf73d959257bed
resilientcoders6 <resilientcoders6@resilientcoders6s-MacBook-Air.local>
2017-03-30 12:21:49 -0400

    created expenses form

--- public/controllers/ExpensesController.js
+++ public/controllers/ExpensesController.js
@@ -8,13 +8,21 @@
      * Controls the flow of data for the expenses-annual view
      * @constructor
      */
-    function ExpensesAnnualController() {
+    function ExpensesAnnualController(){
         let vm = this;
+        vm.toggleModal=toggleModal;
+        vm.showModal = false;
 
         function init() {
             console.log("Expenses Annual Controller loaded");
         }
         init();
+
+        function toggleModal(){
+          console.log("works");
+          vm.showModal=!vm.showModal
+        }
+
     }
 
     /**
