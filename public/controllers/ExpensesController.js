c2a1211c6f4152d6b9cac3609648cadec05d5198
Muigai <muigai@Muigais-MacBook-Air.local>
2017-04-03 13:00:55 -0400

    merged backend services and working on client to server side connection

--- public/controllers/ExpensesController.js
+++ public/controllers/ExpensesController.js
@@ -1,33 +1,41 @@
 (function() {
-    angular
-        .module("BeautyBooks")
-        .controller("ExpensesAnnualController", ExpensesAnnualController)
-        .controller("ExpensesQuarterlyController", ExpensesQuarterlyController);
+  angular
+    .module("BeautyBooks")
+    .controller("ExpensesAnnualController", ExpensesAnnualController)
+    .controller("ExpensesQuarterlyController", ExpensesQuarterlyController);
 
-    /**
-     * Controls the flow of data for the expenses-annual view
-     * @constructor
-     */
-    function ExpensesAnnualController() {
-        let vm = this;
+  /**
+   * Controls the flow of data for the expenses-annual view
+   * @constructor
+   */
+  function ExpensesAnnualController(){
+    let vm = this;
+    vm.toggleModal=toggleModal;
+    vm.showModal = false;
 
-        function init() {
-            console.log("Expenses Annual Controller loaded");
-        }
-        init();
+    function init() {
+      console.log("Expenses Annual Controller loaded");
+    }
+    init();
+
+    function toggleModal(){
+      console.log("works");
+      vm.showModal=!vm.showModal
     }
 
-    /**
-     * Controls the flow of data for the expenses-quarterly view
-     * @constructor
-     */
-    function ExpensesQuarterlyController() {
-        let vm = this;
+  }
 
-        function init() {
-            console.log("Expenses Quarterly Controller loaded");
-        }
-        init();
+  /**
+   * Controls the flow of data for the expenses-quarterly view
+   * @constructor
+   */
+  function ExpensesQuarterlyController() {
+    let vm = this;
+
+    function init() {
+      console.log("Expenses Quarterly Controller loaded");
     }
+    init();
+  }
 
 })();
